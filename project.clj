f635e23bceb5b15d794fedd70b4fd5b9c8113cb3
Chen Li <chencassc@gmail.com>
2014-09-25 11:06:28 +0800

    Fix deps

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/data.zip "0.1.1"]
                  [clj-http "0.9.2"
-                  :exclusions [cheshire crouton slingshot]]
+                  :exclusions [cheshire crouton ]]
                  [hiccup "1.0.5"]
                  [hickory "0.5.3"
                   :exclusions [org.clojure/clojurescript]]
